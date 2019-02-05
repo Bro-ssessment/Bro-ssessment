dcf29cd29a7f0aaf3696428f7f76e7c200a29bb3
Michael Lin <michael.lin1@ibm.com>
2019-02-05 11:48:55 -0700

    add db models

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Bro-ssessment
+
+## Development
+
+```sh
+# Setup python virtual environment
+$ pip3 install --user virtualenv
+$ virtualenv venv --python=python3
+$ source venv/bin/activate
+
+# Install python dependencies
+$ pip install -r requirements.txt
+
+# Setup environment variable
+$ cp .env.example .env
+$ source .env
+```
---
+++ brossessment/models.py
@@ -0,0 +1,39 @@
+from peewee import *
+from brossessment.config import *
+
+# pylint: disable=invalid-name
+postgres_db = PostgresqlDatabase(POSTGRES_DB, host=POSTGRES_HOST, port=POSTGRES_PORT,
+                                 user=POSTGRES_USER, password=POSTGRES_PASSWORD)
+# pylint: enable=invalid-name
+
+
+class User(Model):
+    user_id = BigIntegerField(primary_key=True)
+    average_sentiment_score = DecimalField(decimal_places=3)
+
+    class Meta:
+        database = postgres_db
+        table_name = 'users'
+
+
+class Class(Model):
+    class_id = BigIntegerField(primary_key=True)
+    average_sentiment_score = DecimalField(decimal_places=3)
+
+    class Meta:
+        database = postgres_db
+        table_name = 'classes'
+
+
+class Post(Model):
+    post_id = BigIntegerField(primary_key=True)
+    class_id = ForeignKeyField(Class, null=True)
+    user_id = ForeignKeyField(User, null=True)
+    build_on = ForeignKeyField('self')
+    average_sentiment_score = DecimalField(decimal_places=3)
+    title = TextField()
+    content = TextField()
+
+    class Meta:
+        database = postgres_db
+        table_name = 'posts'
