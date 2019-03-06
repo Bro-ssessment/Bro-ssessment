1a2a724fdb892afdf09398db988773a300fa77a0
tyheise <tyler.heise@gmail.com>
2019-03-06 00:30:14 -0700

    Refactored code to be more modular and added functionality to spell check.

--- excelParser.py
+++ excelParser.py
@@ -1,8 +1,10 @@
 import sys
 import xlrd
 import os
+from datetime import datetime
 from xlrd import open_workbook
 from bs4 import BeautifulSoup
+from symspellpy.symspellpy import SymSpell
 from brossessment.models import *
 
 # Funtion to get the column lables
@@ -25,19 +27,34 @@ def stripHTML(HTMLstring):
     strippedString = soup.get_text()
     return strippedString
 
-def parseSheet(filePath, classid):
+def initializeSymSpell(maxEditDistance):
+    # this variable can be between 5-7, the hgiher the faster the aglorithim runs but the more memory consumption
+    prefixLength = 7
+    # create object
+    symSpell = SymSpell(maxEditDistance, prefixLength)
+    # load dictionary
+    dictionaryPath = os.path.join(os.path.dirname(__file__),
+                                   "frequency_dictionary_en_82_765.txt")
+    termIndex = 0  # column of the term in the dictionary text file
+    countIndex = 1  # column of the term frequency in the dictionary text file
+    if not symSpell.load_dictionary(dictionaryPath, termIndex, countIndex):
+        print("Dictionary file not found")
+        return
+    return symSpell
 
-    #create class for this workbook
-    query = Class.select().where(Class.class_id == classid)
-    if not query.exists():
-        course = Class.create(class_id=classid, average_sentiment_score=0)
-    # print(course)
+def spellCheck(symSpell, maxEditDistance, string):
+    suggestions = symSpell.lookup_compound(string, maxEditDistance)
+    #print(suggestions[0].term)
+    return suggestions[0].term
 
-    wb = open_workbook(filePath)
+def cleanContent(symSpell, maxEditDistance, content):
+    content = stripHTML(content)
+    content = spellCheck(symSpell, maxEditDistance, content)
+
+def getRowList(wb, symSpell, maxEditDistance):
     sheet = wb.sheet_by_index(2)
     labels = getSheetLabels(sheet)
 
-    userSet = set()
     labelToCellList = []
     for row in range(1,sheet.nrows):
         # Get a list of cell values for the row
@@ -55,14 +72,48 @@ def parseSheet(filePath, classid):
                     cell = None
             labelToCell[label] = cell
 
-        userSet.add(labelToCell["PersonID"])
-
         if labelToCell["BuildsOn"] == 0:
             labelToCell["BuildsOn"] == None
+
+        # private can equal 0 or 1, the excel sheets have 0, 1, and 2 for some reason
+        if labelToCell["Private"] != 1 or 0:
+            labelToCell["Private"] = 1
+
+        if labelToCell["SharedFlag"] != 1 or 0:
+            labelToCell["SharedFlag"] = 1
+
         content = labelToCell["NoteContents"]
         if content is None:
+            content == ""
             labelToCell["NoteContents"] = ""
+
+        print(labelToCell["NoteID"])
+
+        content = cleanContent(symSpell, maxEditDistance, content)
+        labelToCell["NoteContents"] = content
         labelToCellList.append(labelToCell)
+
+    return labelToCellList
+
+def getUserSet(rowList):
+    userSet = set()
+    for row in rowList:
+        userSet.add(row["PersonID"])
+
+    return userSet
+
+def parseSheet(filePath, classid, symSpell, maxEditDistance):
+
+    #create class for this workbook
+    query = Class.select().where(Class.class_id == classid)
+    if not query.exists():
+        course = Class.create(class_id=classid, average_sentiment_score=0)
+    # print(course)
+
+    wb = open_workbook(filePath)
+    labelToCellList = getRowList(wb, symSpell, maxEditDistance)
+    #print(labelToCellList)
+    userSet = getUserSet(labelToCellList)
 
     # Make users
     with postgres_db.atomic():
@@ -78,24 +129,18 @@ def parseSheet(filePath, classid):
             user_id = labelToCell["PersonID"]
             # Get post values
             post_id = labelToCell["NoteID"]
+            print(post_id)
             class_id = classid
             #build_on = labelToCell["BuildsOn"]
             title = labelToCell["Title"]
-            content = stripHTML(labelToCell["NoteContents"])
+            content = labelToCell["NoteContents"]
             topic_id = labelToCell["TopicID"]
-
-            # private can equal 0 or 1, the excel sheets have 0, 1, and 2 for some reason
             private = labelToCell["Private"]
-            if private == 2:
-                private = 1
-
-            shared = labelToCell["Shared"]
-            if shared == 2:
-                shared = 1
+            shared = labelToCell["SharedFlag"]
 
             #print(post_id)
 
-            query = Post.insert(post_id=post_id, class_id=class_id, user_id=user_id, title=title, content=content, topic_id=topic_id)
+            query = Post.insert(post_id=post_id, class_id=class_id, user_id=user_id, title=title, content=content, topic_id=topic_id, private=private, shared=shared)
             query.execute()
 
     with postgres_db.atomic():
@@ -107,35 +152,32 @@ def parseSheet(filePath, classid):
                 query = Post.update(builds_on=builds_on).where(Post.post_id==post_id)
                 query.execute()
 
-
-
-
-        # Get post values
-        # post_id = labelToCell["NoteID"]
-        # class_id = classid
-        # user_id = labelToCell["PersonID"]
-        # build_on = labelToCell["BuildsOn"]
-        # title = labelToCell["Title"]
-        # content = stripHTML(noteContent)
-        # topic_id = labelToCell["TopicID"]
-        #
-        # print(post_id)
-        # post = Post.create(post_id=post_id, class_id=class_id, user_id=user_id, build_on=build_on, title=title, content=content, topic_id=topic_id)
-
     return
 
+def main(argv):
 
-def main():
+    if len(argv) != 1:
+        print("correct usage is python excelParser.py *maxEditDistanceInterger*")
+        return -1
+    else:
+        maxEditDistance = int(argv[0])
+
     rowList = []
     path = os.path.join('spreadsheets')
     classid = 0
+
+    symSpell = initializeSymSpell(maxEditDistance)
     for root, dirs, files in os.walk(path):
         for filename in files:
+            tstart = datetime.now()
             classid += 1
             filePath = os.path.join('spreadsheets', filename)
             print("Now parsing "+ filename)
-            row = parseSheet(filePath, classid)
+            row = parseSheet(filePath, classid, symSpell, maxEditDistance)
+            tend = datetime.now()
+            time = tend - tstart
             print("Finished")
+            print(time.seconds)
             print()
             # rowList.append(row)
 
@@ -150,4 +192,4 @@ def main():
     return 0
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
