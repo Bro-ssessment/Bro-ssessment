137ec41e19d820f51454bb25a288d8f5f75be444
tyheise <tyler.heise@gmail.com>
2019-03-11 12:05:15 -0600

    Merge Excel parser into master (#17)
    * Added topicId to models.
    
    * Fixed builds_on typo.
    
    * re-fixed the builds_on typo haha.
    
    * Fixed add column typo.
    
    * Added HTML parsing lib
    
    * Parser almost fully functioning. Currently dealing with a bug with the builds_on field in Posts model. Code is messy right now, needs to be cleaned up and split into better methods.
    
    * add rename builds_on column script
    
    * Added related name to builds_on field
    
    * Parser fully functioning. Need to add instructions and clean up the code (maybe) not sure if its worth it.
    
    * Added db migration for a private column
    
    * Added non functioning code to insert private column.
    
    * Added private and shared columns.
    
    * Added frequency dictionary for spell checking
    
    * Program to test time of spell checking algs.
    
    * Refactored code to be more modular and added functionality to spell check.
    
    * added symspell requirement
    
    * Changed speel check functionality to only run when a command line argument is given.
    
    * Added wordcount field to posts table.
    
    * Added wordcount to parser.
    
    * replyQuery.sql
    
    * Changed reply query to create a view.
    
    * Made column names more explicit.
    
    * Added views for some statistics.

--- replyQuery.sql
+++ replyQuery.sql
@@ -1,5 +1,7 @@
 CREATE VIEW replies AS
-SELECT r3.replyer, r3.replied_to, count(*), sum(r3.wordcount), avg(r3.bro_sentiment)
+SELECT r3.replyer as replier_id, r3.replied_to as replied_to_id, count(*) as number_of_replies,
+       sum(r3.wordcount) as total_wordcount, avg(r3.wordcount) as average_wordcount, avg(r3.bro_sentiment) as average_bro_sentiment_score,
+       max(r3.bro_sentiment) as max_bro_sentiment_score, min(r3.bro_sentiment) as min_bro_sentiment_score
 FROM
     (SELECT p1.post_id as replyer_post_id, p1.user_id as replyer, p2.user_id as replied_to, p1.wordcount, p1.bro_sentiment
     FROM posts p1, posts p2
---
+++ userStatisticsQuery.sql
@@ -0,0 +1,6 @@
+CREATE VIEW user_statistics as
+SELECT user_id, count(*) as number_of_posts, sum(wordcount) as total_wordcount,
+       avg(wordcount) as average_wordcount, avg(bro_sentiment) as average_bro_sentiment_score,
+       max(bro_sentiment) as max_bro_sentiment_score, min(bro_sentiment) as min_bro_sentiment_score
+FROM posts
+GROUP BY user_id;
