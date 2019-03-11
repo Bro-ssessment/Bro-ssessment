c6325975c10bc347241e8289ec0034532f36102b
tyheise <tyler.heise@gmail.com>
2019-03-10 19:54:28 -0600

    Changed reply query to create a view.

--- replyQuery.sql
+++ replyQuery.sql
@@ -1,3 +1,4 @@
+CREATE VIEW replies AS
 SELECT r3.replyer, r3.replied_to, count(*), sum(r3.wordcount), avg(r3.bro_sentiment)
 FROM
     (SELECT p1.post_id as replyer_post_id, p1.user_id as replyer, p2.user_id as replied_to, p1.wordcount, p1.bro_sentiment
